e4fb45a91f237ceb47661cd849b0c2d491d31f5c
Irwan Akhmad <irwanakhmadzainni@gmail.com>
2025-07-19 20:00:59 +0700

    Tambah text paragraph

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,13 +10,8 @@
     <h1>Welcome to the Home Page</h1>
     <p>This is the home page of the application.</p>
     <p>Feel free to explore!</p>
-    <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
-        </ul>
-    </nav>
+    <p>APA YAAAA</p>
+
     <footer>
         <p>&copy; 2023 Your Company Name</p>
     </footer>
