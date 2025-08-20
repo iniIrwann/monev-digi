5ed565babd046ad95d7c4ac9cb0e9c073e241937
Xxyzarz7 <zamzamhidayattullah27@gmail.com>
2025-08-20 15:03:04 +0700

    Coba

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,33 +19,103 @@ class DatabaseSeeder extends Seeder
         //     MonevSeeder::class,
         // ]);
         User::create([
-            'name' => 'Admin Desa Cileunyi Wetan',
-            'username' => 'admin2',
+            'name' => 'Admin Kecamatan Soreang',
+            'username' => 'kecamatan_soreang',
+            'role' => 'kecamatan',
+            'nohp' => '0812346789',
+            'desa' => 'Kecamatan Soreang',
+            'email' => 'kecamatan@example.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        User::create([
+            'name' => 'Admin Desa Cingcin',
+            'username' => 'desa_cingcin',
             'role' => 'desa',
             'nohp' => '08123456789',
-            'desa' => 'Desa Cileunyi Wetan',
-            'email' => 'admin1@example.com',
-            'password' => Hash::make('123'),
+            'desa' => 'Desa Cingcin',
+            'email' => 'desa@example.com',
+            'password' => Hash::make('12345678'),
         ]);
-
         User::create([
-            'name' => 'Admin Desa Cileunyi Kulon',
-            'username' => 'admin3',
+            'name' => 'Admin Desa Karamatmulya',
+            'username' => 'desa_karamatmulya',
             'role' => 'desa',
-            'nohp' => '082233445566',
-            'desa' => 'Desa Cileunyi Kulon',
-            'email' => 'admin2@example.com',
-            'password' => Hash::make('123'),
+            'nohp' => '08123456789',
+            'desa' => 'Desa Karamatmulya',
+            'email' => 'desa@example.com',
+            'password' => Hash::make('12345678'),
         ]);
-
         User::create([
-            'name' => 'Admin Kecamatan Cileunyi',
-            'username' => 'admin',
-            'role' => 'kecamatan',
-            'nohp' => '082233445566',
-            'desa' => 'Kecamatan Cileunyi',
-            'email' => 'admin_kec@example.com',
-            'password' => Hash::make('pass'),
+            'name' => 'Admin Desa Pamekaran',
+            'username' => 'desa_pamekaran',
+            'role' => 'desa',
+            'nohp' => '08123456789',
+            'desa' => 'Desa Pamekaran',
+            'email' => 'desa@example.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        User::create([
+            'name' => 'Admin Desa Panyirapan',
+            'username' => 'desa_panyirapan',
+            'role' => 'desa',
+            'nohp' => '08123456789',
+            'desa' => 'Desa Panyirapan',
+            'email' => 'desa@example.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        User::create([
+            'name' => 'Admin Desa Parungserab',
+            'username' => 'desa_parungserab',
+            'role' => 'desa',
+            'nohp' => '08123456789',
+            'desa' => 'Desa Parungserab',
+            'email' => 'desa@example.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        User::create([
+            'name' => 'Admin Desa Sadu',
+            'username' => 'desa_sadu',
+            'role' => 'desa',
+            'nohp' => '08123456789',
+            'desa' => 'Desa Sadu',
+            'email' => 'desa@example.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        User::create([
+            'name' => 'Admin Desa Sekarwangi',
+            'username' => 'desa_sekarwangi',
+            'role' => 'desa',
+            'nohp' => '08123456789',
+            'desa' => 'Desa Sekarwangi',
+            'email' => 'desa@example.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        User::create([
+            'name' => 'Admin Desa Soreang',
+            'username' => 'desa_soreang',
+            'role' => 'desa',
+            'nohp' => '08123456789',
+            'desa' => 'Desa Soreang',
+            'email' => 'desa@example.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        User::create([
+            'name' => 'Admin Desa Sukajadi',
+            'username' => 'desa_sukajadi',
+            'role' => 'desa',
+            'nohp' => '08123456789',
+            'desa' => 'Desa Sukajadi',
+            'email' => 'desa@example.com',
+            'password' => Hash::make('12345678'),
+        ]);
+        User::create([
+            'name' => 'Admin Desa Sukanagara',
+            'username' => 'desa_sukanagara',
+            'role' => 'desa',
+            'nohp' => '08123456789',
+            'desa' => 'Desa Sukanagara',
+            'email' => 'desa@example.com',
+            'password' => Hash::make('12345678'),
         ]);
     }
 }
